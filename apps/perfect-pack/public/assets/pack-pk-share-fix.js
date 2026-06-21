716356fa99541f7c443fc473f27756af62d2c350
0xbobot <0xbobot@gmail.com>
2026-06-21 14:41:13 +0800

    Add PK share fix source loader

---
+++ apps/perfect-pack/public/assets/pack-pk-share-fix.js
@@ -0,0 +1,6 @@
+(() => {
+  const script = document.createElement("script");
+  script.src = "/play-pack/assets/pack-pk-share-fix.js";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
